ad61ff2935793cc7a87b967de83f1c2320fb6aed
Scott Russell <scrussell24@gmail.com>
2016-05-30 16:51:18 -0500

    removed stupid comment

--- src/wiki2text.js
+++ src/wiki2text.js
@@ -1,65 +1,7 @@
 var bot = require('nodemw'),
     Parsoid = require('parsoid'),
-    html2text = require('html-to-text');//,
-    //client;
+    html2text = require('html-to-text');
     
-// module.exports = {
-//     convert : function(article){
-        
-//         return new Promise(function(resolve,reject){
-//             if(!client){
-//                 throw error('Client not initialized. see setWiki.');
-//             }
-            
-//             client.getArticle(article, function(err, data) {
-//                 if (err) {
-//                     reject(Error(err));
-//                 }
-//                 Parsoid.parse(data, { document: true }).then(function(res) {
-//                     resolve(html2text.fromString(res.out.outerHTML, {
-//                         wordwrap: 100
-//                     }));
-//                 }).done();
-//             });
-//         });
-//     },
-//     setWiki: function(url, path){
-//         client = new bot({
-//             server: url,
-//             path: path,
-//             debug: false
-//         });
-//     }
-// }
-
-// module.exports = function(url, path){
-    
-//     this.client =  new bot({
-//             server: url,
-//             path: path,
-//             debug: false
-//     });
-    
-//     return {
-//         convert = function(article){ 
-//             return new Promise(function(resolve,reject){
-                
-//                 this.client.getArticle(article, function(err, data) {
-//                     if (err) {
-//                         reject(Error(err));
-//                     }
-//                     Parsoid.parse(data, { document: true }).then(function(res) {
-//                         resolve(html2text.fromString(res.out.outerHTML, {
-//                             wordwrap: 100
-//                         }));
-//                     }).done();
-//                 });
-//             });
-//         }
-//     }
-// }
-
-
 var Wiki2Text = function (url, path) {
     
     this.client =  new bot({
